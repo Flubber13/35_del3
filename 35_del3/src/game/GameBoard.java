7804e7e793ba6793e3b1f9d83961658505e60c6b
Oliver Elleman Thomsen <Oliver.e.t@hotmail.com>
2015-11-11 15:09:56 +0100

    .

--- 35_del3/src/game/GameBoard.java
+++ 35_del3/src/game/GameBoard.java
@@ -3,8 +3,6 @@ package game;
 
 public class GameBoard {
 	
-	/*When the dice is rolled in the game the amount of points added is pulled from this array. 
-	 */
+	pu
 	
-	public static int[] field = {0,0,250,-100,100,-20,180,0,-70,60,-80,-50,650};
 }
