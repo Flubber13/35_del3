d9c3153cb3cf909940f305a8434dbc98ff38a6ee
Sofie Larsen <sofie_p_larsen@hotmail.com>
2015-11-11 15:17:46 +0100

    Omdøbte klassen "Refudge" til "Refuge".

---
+++ 35_del3/src/fields/Refuge.java
@@ -0,0 +1,19 @@
+package fields;
+
+import game.Player;
+
+public class Refuge extends Field {
+
+	int bonus;
+	
+	
+	@Override
+	public void landedOn(Player player) {
+				
+	}
+
+	
+	
+//	12.Walled city Refuge Receive 5000  
+//	13.Monastery Refuge Receive 500  	
+}
