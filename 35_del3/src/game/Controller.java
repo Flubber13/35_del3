e9923b0b5ab3be16ad011d5240c9638e97f474c0
Oliver Elleman Thomsen <Oliver.e.t@hotmail.com>
2015-11-12 15:25:44 +0100

    modificeret spillepladen og tilføjet en metode der initializer den i controlleren

--- 35_del3/src/game/Controller.java
+++ 35_del3/src/game/Controller.java
@@ -9,6 +9,9 @@ public class Controller {
 	int numOfPlayers;
 
 	public void run() {
+		
+		GameBoard.initialize();
+		
 		// Determines the number of players in the game
 		numOfPlayers = GUI.getUserInteger(Text.howManyPlayers, 2, 6);
 		
@@ -23,7 +26,7 @@ public class Controller {
 		// Creates a new dice cup
 		dicecup = new DiceCup();
 		
-		//this loop gives all active players a turn  
+		// This loop gives all active players a turn  
 		for (int i = 0; i < numOfPlayers; i++) {
 			playerTurn(player[i]);
 		}
@@ -33,7 +36,7 @@ public class Controller {
 	// Method for the player turn
 	private void playerTurn(Player player) {
 		GUI.showMessage(player.getName() + Text.roll );									//The player is asked to roll the dice
-		player.setPosition(dicecup.roll());													//dice is rolled
+		player.setPosition(dicecup.roll());												//dice is rolled
 		GUI.setCar(player.getPosition(), player.getName());
 		GUI.setDice(dicecup.getDie1().getLastRoll(), dicecup.getDie2().getLastRoll());	//dice is shown on screen
 		GUI.showMessage(player.getName() + Text.landedOn);								//Displays which player landed on the field
--- 35_del3/src/game/GameBoard.java
+++ 35_del3/src/game/GameBoard.java
@@ -5,69 +5,78 @@ import desktop_resources.GUI;
 public class GameBoard {
 	
 	
-	public void makeGameBord(){
+	public static void initialize(){
 		
 		GUI.setSubText(1, null); // Start skal ikke umiddelbart have nogen text, og Title er allerede "START"
 		
-		GUI.setTitleText(2, "");
+		GUI.setTitleText(2, "Mountain");
 		GUI.setSubText(2, "");
 		
-		GUI.setTitleText(3, "");
+		GUI.setTitleText(3, "Gold Mine");
 		GUI.setSubText(3, "");
 		
-		GUI.setTitleText(4, "");
+		GUI.setTitleText(4, "Crater");
 		GUI.setSubText(4, "");
 		
-		GUI.setTitleText(5, "");
+		GUI.setTitleText(5, "Second Sail");
 		GUI.setSubText(5, "");
 		
-		GUI.setTitleText(6, "");
+		GUI.setTitleText(6, "Cold Dessert");
 		GUI.setSubText(6, "");
 		
-		GUI.setTitleText(7, "");
+		GUI.setTitleText(7, "Monastery");
 		GUI.setSubText(7, "");
 		
-		GUI.setTitleText(8, "");
+		GUI.setTitleText(8, "Tribe Encapment");
 		GUI.setSubText(8, "");
 		
-		GUI.setTitleText(9, "");
+		GUI.setTitleText(9, "Sea Grover");
 		GUI.setSubText(9, "");
 		
-		GUI.setTitleText(10, "");
+		GUI.setTitleText(10, "Black Cave");
 		GUI.setSubText(10, "");
 		
-		GUI.setTitleText(11, "");
+		GUI.setTitleText(11, "The Pit");
 		GUI.setSubText(11, "");
 		
-		GUI.setTitleText(12, "");
+		GUI.setTitleText(12, "Castle");
 		GUI.setSubText(12, "");
 		
-		GUI.setTitleText(13, "");
+		GUI.setTitleText(13, "Caraven");
 		GUI.setSubText(13, "");
 		
-		GUI.setTitleText(14, "");
+		GUI.setTitleText(14, "The Werewall");
+		GUI.setSubText(14, "");
+		
+		GUI.setTitleText(15, "Buccaneers");
 		GUI.setSubText(15, "");
 		
-		GUI.setTitleText(16, "");
+		GUI.setTitleText(16, "Mountain Village");
 		GUI.setSubText(16, "");
 		
-		GUI.setTitleText(17, "");
+		GUI.setTitleText(17, "Walled City");
 		GUI.setSubText(17, "");
 		
-		GUI.setTitleText(18, "");
+		GUI.setTitleText(18, "South Citadel");
 		GUI.setSubText(18, "");
 		
-		GUI.setTitleText(19, "");
+		GUI.setTitleText(19, "Huts in the mountain");
 		GUI.setSubText(19, "");
 		
-		GUI.setTitleText(20, "");
+		GUI.setTitleText(20, "Palace Gates");
 		GUI.setSubText(20, "");
 		
-		GUI.setTitleText(21, "");
+		GUI.setTitleText(21, "Privateer Armade");
 		GUI.setSubText(21, "");
 		
-		GUI.setTitleText(22, "");
+		GUI.setTitleText(22, "Tower");
 		GUI.setSubText(22, "");
+	
+		for (int i = 23; i < 40; i++) {
+			GUI.setTitleText(i, "");
+			GUI.setSubText(i, "");
+		}
+	
 	}
 
 	
