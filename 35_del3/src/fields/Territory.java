1b5778c6403a0c9e1ca3f4a3c593ab1a58a78151
Sofie <Sofie@192.168.75.1>
2015-11-13 11:34:15 +0100

    Tilføjede i metoden landedOn i Territory.

--- 35_del3/src/fields/Territory.java
+++ 35_del3/src/fields/Territory.java
@@ -1,12 +1,18 @@
 package fields;
 
+import desktop_resources.GUI;
 import game.Player;
 
 public class Territory extends Ownables {
 
-	
+
 	int rent;
-	
+	int i;
+	int j;
+
+	int[] territoryPrice = {0, 2000, 1500, 3000, 1000, 4000, 8000, 4300, 4750, 5000, 5500, 6000};
+	int[] territoryRent = {0, 500, 300, 700, 100, 1000, 4000, 1300, 1600, 2000, 2600, 3200};
+
 	@Override
 	public int getRent() {
 		return rent;
@@ -14,20 +20,37 @@ public class Territory extends Ownables {
 
 	@Override
 	public void landedOn(Player player) {
-		// TODO Auto-generated method stub
-		
+		this.i = player.getPosition();
+
+		// j er spillerens position i ovenstående index. Hvis eksempelvis spilleren befinder sig på 
+		// felt nr. 4, så vil det svare til index 2 (både i Price og Rent). 
+		this.j = i/2;
+
+		if ((i == 2) || (i == 4) || (i == 6) || (i == 8) ||
+				(i == 10) || (i == 12) || (i == 14) || (i == 16) ||
+				(i == 18) ||(i == 20) || (i == 22)){
+
+			// Hvis det pågældende felt er ejet (mangler at udføre klassen).
+			if(i == 0){
+				player.getAccount().addBalance(- territoryRent[j]);
+			}
+			// Ellers bliver spilleren spurgt, om vedkommende vil købe feltet.
+			else{
+				boolean Buttonpressed = GUI.getUserLeftButtonPressed("Feltet er ikke ejet. Vil du købe det for: "
+						+ territoryPrice[j] + " ?", "Ja", "Nej");{
+							
+							// Hvis spilleren siger ja, købes feltet.
+							if (Buttonpressed == true){
+								player.getAccount().addBalance(- territoryPrice[j]);
+								
+								// Tilføjer derefter feltet til spilleren.
+							}
+						}
+
+			}
+
+		}
+
 	}
 
-	
-//	1. Tribe Encampment Territory Rent 100 Price 1000  
-//	2. Crater Territory Rent 300 Price 1500  
-//	3. Mountain Territory Rent 500 Price 2000  
-//	4. Cold Desert Territory Rent 700 Price 3000  
-//	5. Black cave Territory Rent 1000 Price 4000  
-//	6. The Werewall Territory Rent 1300 Price 4300  
-//	7. Mountain village Territory Rent 1600 Price 4750  
-//	8. South Citadel Territory Rent 2000 Price 5000  
-//	9. Palace gates Territory Rent 2600 Price 5500  
-//	10.Tower Territory Rent 3200 Price 6000  
-//	11.Castle Territory Rent 4000 Price 8000  
 }
